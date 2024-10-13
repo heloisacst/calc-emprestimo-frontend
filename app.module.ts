be17ace85192f7ebe3b334b05c246fd63e3b534b
Heloisa Cristina dos Santos <heloisa.santos@zitrus.com.br>
2024-10-13 20:47:22 -0300

    inicializacao projeto

---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+//import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './src/app/app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
