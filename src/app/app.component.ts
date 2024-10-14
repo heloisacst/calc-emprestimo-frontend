5a35efa1e8eba0091c7712ca6c37978ddc1ee30b
Heloisa Cristina dos Santos <heloisa.santos@zitrus.com.br>
2024-10-14 12:30:59 -0300

    ajustes na tabela para retornar os dados da API + estilizacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { BuscaApiService } from '../service/busca-api.service';
 import { Subscription } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { DadosEmprestimo } from '../models/dados-emprestimo.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [FormsModule, HttpClientModule]
+  imports: [FormsModule, HttpClientModule, CommonModule]
 })
 
 export class AppComponent implements OnInit{
@@ -17,16 +19,22 @@ export class AppComponent implements OnInit{
   subscription: Subscription | undefined;
   dados: any[] = [];
   body: any = '';
-
+  dadosEmprestimo: DadosEmprestimo | any;
   dataInicial: string = '';
   dataFinal: string = '';
   dataPrimeiroPagamento: string = '';
   valorEmprestimo: number = 0;
   taxaJuros: number = 0;
+  msgErro: string = '';
 
   constructor(private apiService: BuscaApiService){}
 
   calcularEmprestimo() {
+
+    if (!this.validarDatas()) {
+      return;
+    }
+
     this.body = this.apiService.montarBody(
       this.dataInicial,
       this.dataFinal,
@@ -43,6 +51,7 @@ export class AppComponent implements OnInit{
           {
             next: (data: any[]) => {
                 this.dados = data;
+                this.dadosEmprestimo = data;
             },
             error: (erro) => {
                     console.log(erro)
@@ -51,6 +60,25 @@ export class AppComponent implements OnInit{
       );
       console.log(this.dados);
   }
+
+  validarDatas(): boolean {
+    const inicial = new Date(this.dataInicial);
+    const final = new Date(this.dataFinal);
+    const primeiroPagamento = new Date(this.dataPrimeiroPagamento);
+
+    if (final <= inicial) {
+        this.msgErro = 'A data final deve ser maior que a data inicial.';
+        return false;
+    }
+    if (primeiroPagamento <= inicial || primeiroPagamento >= final) {
+        this.msgErro = 'A data de primeiro pagamento deve ser maior que a data inicial e menor que a data final.';
+        return false;
+    }
+
+    this.msgErro= '';
+    return true;
+  }
+
   ngOnInit(): void {
     this.dataInicial = '2024-01-01';
     this.dataFinal = '2034-01-01';
---
+++ src/models/dados-emprestimo.model.ts
@@ -0,0 +1,13 @@
+export interface DadosEmprestimo {
+  dataCompetencia: string;
+  saldoDevedor: number;
+  consolidado: string;
+  total: number;
+  amortizacao: number;
+  saldo: number;
+  valorProvisao: number;
+  valorAcumulado: number;
+  valorPago: number;
+  qtdParcelas: number;
+  baseDias: number;
+}
